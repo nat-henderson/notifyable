def639e3a074cadbb5a73220d63fbd2547618a1c
Nathan McKinley <ndm25@case.edu>
2013-07-27 22:37:38 +0000

    dont need that config; config.json is the way

--- src/config/twitter.py
+++
@@ -1,6 +0,0 @@
-CONSUMER_KEY = "H8dkaG4d02PxLG6ft4LsQ"
-CONSUMER_SECRET = "EzZk4LV8S3yxCLSoOmX2a3PtTNvsmMp2btz2sNgzPY"
-
-REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token"
-AUTHORIZE_URL = "https://api.twitter.com/oauth/authorize"
-ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token"
