d18d991633f31e111b164d3487780001c91cd7bb
Shrihari Murlidharan <hariwashere@gmail.com>
2013-07-27 16:37:21 -0700

    Added user tweets streaming

--- src/api_readers/twitter_reader.py
+++ src/api_readers/twitter_reader.py
@@ -1,5 +1,7 @@
 from api_readers.api_reader_daemon import APIReaderDaemon
 import tweepy
+from tweepy.streaming import StreamListener
+from tweepy import Stream
 
 class TwitterReader(APIReaderDaemon):
     consumer_key = "E4TdVoiMVYf44Lvq8KXw"
@@ -9,6 +11,23 @@ class TwitterReader(APIReaderDaemon):
     secret = "jTtwH8ixH1ImbmAMMyCWTwzYU928m4k40DRFwLe2coQ"
 
     def start(self):
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(key, secret)
+        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        auth.set_access_token(self.key, self.secret)
+        listener = StdOutListener()
+        stream = Stream(auth, listener)
+        stream.userstream()
 
+class StdOutListener(StreamListener):
+    """ A listener handles tweets are the received from the stream.
+    This is a basic listener that just prints received tweets to stdout.
+
+    """
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status):
+        print status
+
+if __name__=="__main__":
+    TwitterReader().start()
