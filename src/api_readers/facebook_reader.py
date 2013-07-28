a65273ca1ca244c58fb437053882aeb978575843
Joshua Schwarz <josh@amagit.com>
2013-07-28 01:18:44 +0000

    Now pulling feed data from Facebook.

--- src/api_readers/facebook_reader.py
+++ src/api_readers/facebook_reader.py
@@ -17,9 +17,9 @@ class FacebookReader(APIReaderDaemon):
     def get_access_token_url(self, code):
         return "https://graph.facebook.com/oauth/access_token?client_id="+self.APP_ID+"&redirect_uri="+self.REDIRECT_URL+"&client_secret="+self.APP_SECRET+"&code="+code
 
-    def get_user_details(self, access_token):
+    def get_data(self, path, access_token):
         list={}
-        url = "https://graph.facebook.com/me?access_token="+access_token;
+        url = "https://graph.facebook.com" + path + "?access_token="+access_token;
         req = urllib2.Request(url)
         try: 
             response= urllib2.urlopen(req)
@@ -33,7 +33,7 @@ class FacebookReader(APIReaderDaemon):
     def start(self):
         print self.get_dialog_url()
         print self.get_access_token_url('AQBFkdQ62e8SKEV5I4T9VuKg7Tj6ximM0cKSqqTqZa7eRaUk_CrMRgpUt0F3z1a8Btj6LWOQC7tOgtQ78fPPhxJ-v9YujyoMkRDBZKmGf0zC_VCC7mf-cbNdJeFfIigh2vIWoKTBPm4jY6lHdqJIVAkz6I1Bg-GWlhSPsBD_drTQZ0otoktCKi6Angwyd7qibQmZ3Ae_QK5FSnoVkKGDPFJH1uvZXX9QAWJ4KAdxRFEgcHlQwuYAtlr1UfmouCt803lk1Gdw5rzJ-ckT_3m34JG7O84c_2A7XNK8shMyY55HZdf6lYjjSef-H_vtpqA_UEM')
-        print self.get_user_details(self.access_token)
+        print self.get_data("/me/feed", self.access_token)
 
 if __name__=="__main__":
     FacebookReader().start()
