05e1b5f98d645b203a89a184e7085d14b20e343b
Daniel Ge <dange@seas.upenn.edu>
2013-07-27 23:05:02 -0700

    Fix render_dashboard
    Bad use of 'self' and use 'current_user' from Flask-Security

--- src/renderers/dashboard.py
+++ src/renderers/dashboard.py
@@ -1,17 +1,20 @@
 from flask import Blueprint
 from flask import render_template
+from flask.ext.login import login_required
+from flask.ext.security.core import current_user
 from models import Session, OAuthTokens
 
 dashboard_renderer = Blueprint('dashboard', __name__)
 
-@dashboard_renderer.route('/dashboard/<int:user_id>')
-def render_dashboard(user_id):
-    if self.authentication_tokens_present(user_id):
+@dashboard_renderer.route('/dashboard/alpha')
+@login_required
+def render_dashboard():
+    if authentication_tokens_present(current_user.id):
         return render_template('dashboard.html')
     else:
         return render_template('fetch_tokens.html')
 
-def authentication_tokens_present(self, user_id):
+def authentication_tokens_present(user_id):
     session = Session()
     oauth_tokens = session.query(OAuthTokens).filter_by(user_id=user_id).one()
     return oauth_tokens
