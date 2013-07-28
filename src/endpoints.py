52971f1ec109e66a07744bf6abd7fed47a8b645d
Daniel Ge <dange@seas.upenn.edu>
2013-07-28 04:26:34 -0700

    Add endpoints for Facebook and Twitter

--- src/endpoints.py
+++ src/endpoints.py
@@ -1,6 +1,9 @@
-from models import RSSFeed, GithubRepo
+from models import RSSFeed, GithubRepo, Status, Tweet
 from renderers.rss import rss_renderer
 from renderers.github import gh_renderer
+from renderers.status import status_renderer
+from renderers.tweet import tweet_renderer
+
 
 class Endpoint(object):
     def __init__(self, name, endpoint, renderer, db_table, relevance_filter = None):
@@ -13,4 +16,6 @@ class Endpoint(object):
 endpoints = [
     Endpoint('RSS', '/rss/%i', rss_renderer, RSSFeed, lambda x: True),
     Endpoint('Github', '/github/%i', gh_renderer, GithubRepo, lambda x:True),
+    Endpoint('Facebook', '/facebook', status_renderer, Status, lambda x:True),
+    Endpoint('Twitter', '/tweets', tweet_renderer, Tweet, lambda x:True),
 ]
