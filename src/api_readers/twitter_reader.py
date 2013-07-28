5aa719c51a19f6353f4ba0d6bdd8f540382db2f7
Shrihari Murlidharan <hariwashere@gmail.com>
2013-07-27 22:50:01 -0700

    Added oauth model and changed the secret + key for twitter

--- src/api_readers/twitter_reader.py
+++ src/api_readers/twitter_reader.py
@@ -12,8 +12,8 @@ class TwitterReader(APIReaderDaemon):
     consumer_key = "E4TdVoiMVYf44Lvq8KXw"
     consumer_secret = "4xzOk0EvhcDcfBPQ0mJU4PwSeXVNWfOE5sifz4XZ0"
 
-    key = "1626313634-H01bjk5cH4YIYjlI5QF25h799YG3F9rnWpg2ykm"
-    secret = "jTtwH8ixH1ImbmAMMyCWTwzYU928m4k40DRFwLe2coQ"
+    key = "1626313634-dnDqYB0nMwxwTE6DsGPBlvT89FIA3Opau5poDUe"
+    secret = "QrN0bzEmEq9zUxQIcKgUJNCvUJmfV1BTwWwabmrIW4"
     user_id = 15
 
     stream = None
--- src/renderers/dashboard.py
+++ src/renderers/dashboard.py
@@ -1,7 +1,17 @@
 from flask import Blueprint
+from flask import render_template
+from models import Session, OAuthTokens
 
 dashboard_renderer = Blueprint('dashboard', __name__)
 
 @dashboard_renderer.route('/dashboard/<int:user_id>')
 def render_dashboard(user_id):
-    return "blarghy blergh"
+    if self.authentication_tokens_present(user_id):
+        return render_template('dashboard.html')
+    else:
+        return render_template('fetch_tokens.html')
+
+def authentication_tokens_present(self, user_id):
+    session = Session()
+    oauth_tokens = session.query(OAuthTokens).filter_by(user_id=user_id).one()
+    return oauth_tokens
