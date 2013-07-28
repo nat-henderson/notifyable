881e083ab65113f7fa1c12db7d2339412601fdd8
Shrihari Murlidharan <hariwashere@gmail.com>
2013-07-27 21:24:22 -0700

    Fixed a goofup...a very bad goofup

--- src/renderers/tweet.py
+++ src/renderers/tweet.py
@@ -2,10 +2,10 @@ from flask import Blueprint
 from models import db, Tweet
 import json
 
-rss_renderer = Blueprint('rss', __name__)
+tweet_renderer = Blueprint('tweet', __name__)
 
-@rss_renderer.route('/tweets/<int:user_id>')
-def get_rss_entry(feed_id):
+@tweet_renderer.route('/tweets/<int:user_id>')
+def get_tweet(feed_id):
     tweet = db.session.query(Tweet).order_by(Tweet.id.desc()).one()
     return json.dumps({'type' : 'text',
             'tweet_text' : tweet.tweet_text,
