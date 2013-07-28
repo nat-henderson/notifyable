ab469da7898c893adf2852a8da842dc7472ae927
Joshua Schwarz <josh@amagit.com>
2013-07-28 04:32:21 +0000

    Renderer for FB. Update to Twitter renderer.

--- src/renderers/tweet.py
+++ src/renderers/tweet.py
@@ -5,7 +5,7 @@ import json
 tweet_renderer = Blueprint('tweet', __name__)
 
 @tweet_renderer.route('/tweets/<int:user_id>')
-def get_tweet(feed_id):
+def get_tweet(user_id):
     tweet = db.session.query(Tweet).order_by(Tweet.id.desc()).one()
     return json.dumps({'type' : 'text',
             'tweet_text' : tweet.tweet_text,
